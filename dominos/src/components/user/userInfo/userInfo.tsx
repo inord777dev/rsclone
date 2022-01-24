ba44dc8cf910e8dbebcd41b50d7ebe5bb12cb08a
Kirill Lipnitskiy <kriskip12393@gmail.com>
2022-01-24 16:36:36 +0300

    Удалил лишгие файлы и настроил работу с модулями scss

--- dominos/src/components/user/userInfo/userInfo.tsx
+++ dominos/src/components/user/userInfo/userInfo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import style from './userInfo.module.scss';
 
-export default function UserInfo() {
+export default function UserInfo(): JSX.Element {
   return (
     <div className={style.content}>
       <div className={style.content__title}>Личные данные</div>
