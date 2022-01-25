dfd3d15b3ec51f6fccf58e1af771c71947c942e3
Kirill Lipnitskiy <kriskip12393@gmail.com>
2022-01-25 15:30:30 +0300

    feat: add component Header

--- dominos/src/components/main/main.tsx
+++ dominos/src/components/main/main.tsx
@@ -1,10 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from 'react';
 import style from './main.module.scss';
+import Header from './header/header';
 
 export default function Main() {
   return (
-    <div className={style.content}>
-      Главная
+    <div className={style.wrapper}>
+      <Header />
+      <div className={style.navigation_container}>
+        <nav className={style.navigation}>
+          asdasdasd
+        </nav>
+      </div>
     </div>
   );
 }
