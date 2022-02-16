087b45be7c1af86f5d632795b16b791af145af57
inord777dev <inord777dev@gmail.com>
2022-02-16 15:12:08 +0300

    fix: change statictic start time

--- src/resources/statistics/statistic.db.repository.js
+++ src/resources/statistics/statistic.db.repository.js
@@ -4,6 +4,7 @@ const getOrders = async () => {
   const now = new Date();
   const start = new Date();
   start.setFullYear(start.getFullYear() - 1, 0, 1);
+  start.setHours(0, 0, 0, 0);
   return await UserOrders.find({ date: { $gt: start, $lt: now } });
 };
 
